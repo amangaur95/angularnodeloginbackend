6442a28ad7a683fff98dd241e73d35c0636ea783
amangaur95 <43569264+amangaur95@users.noreply.github.com>
2018-12-03 17:23:22 +0530

    fourth

--- routes/auth.js
+++ routes/auth.js
@@ -46,8 +46,8 @@ router.get('/twitter/callback',
 
 
 passport.use(new LinkedInStrategy({
-    clientID: "86721oa8ii4dxm",
-    clientSecret: "DB8bka0WeolsOIQ3", 
+    clientID: " ", // Enter your LinkedIn clientId
+    clientSecret: " ", // Enter your LinkedIn clientSecret 
     callbackURL: "https://angularnodelogin.herokuapp.com/auth/linkedin/callback",
     scope: ['r_emailaddress', 'r_basicprofile'],
     state: true
