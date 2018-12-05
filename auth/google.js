cb09683d865b387121c63a8374f5755307be965c
amangaur95 <43569264+amangaur95@users.noreply.github.com>
2018-12-05 15:18:07 +0530

    Update google.js

--- auth/google.js
+++ auth/google.js
@@ -3,8 +3,8 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const socialLogin = require('../controllers/sociallogincontroller');
 
 passport.use(new GoogleStrategy({
-    clientID: "358256820106-j7ulil6f3tjuohb3d8jnd2shp3u3kkkk.apps.googleusercontent.com", // Enter your google clientId  
-    clientSecret: "3qdl8WC-7osC54oSARKuo8nz", // Enter your google clientSecret
+    clientID: "", // Enter your google clientId  
+    clientSecret: "", // Enter your google clientSecret
     callbackURL: "https://angularnodelogin.herokuapp.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
